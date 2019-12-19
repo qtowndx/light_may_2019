f9b74918859675bbfa5eddd4dc6ed558f28286e3
sempl <sempl@DESKTOP-VDMNDED>
2019-12-19 04:24:48 +0000

    scripts

---
+++ power_scripts/myscript.ps1
@@ -0,0 +1,6 @@
+#Open files in folder
+invoke-item 'C:\Users\sempl\Google Drive\Website\Devel_notes\*'
+#Open Netbeans
+invoke-item 'C:\Program Files\NetBeans 11.0\bin\netbeans64.exe'
+invoke-item 'C:\wamp64\wampmanager.exe'
+Clear-Host
